289d8d00aec596fa2b868414d1d91afd8c037eb2
Paige Wiener <snowplushie@yahoo.com>
2019-03-05 22:24:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,6 +23,8 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
 
