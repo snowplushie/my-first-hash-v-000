defaeeb709b4b63f68125f5dafa68b7bb4fd10bc
Paige Wiener <snowplushie@yahoo.com>
2019-03-05 22:24:04 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,6 +11,8 @@ def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
 
